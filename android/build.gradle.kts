e096738ba34323927fc4625c8f5f9f1b2ab4a782
hoanvu20041125@gmail.com <hoanvu20041125@gmail.com>
2025-08-03 10:06:45 +0700

    Xay dung chuc nang dang ki dang nhap quen mat khau

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,6 +20,11 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
     dependencies {
         classpath("com.android.tools.build:gradle:8.0.2")
         classpath("com.google.gms:google-services:4.3.15")
