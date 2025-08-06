bb96870d115baa8b3bb133d34e970d1cfeeedf79
Anh Huynh <hma.tech04@gmail.com>
2025-08-06 22:07:05 +0700

    update entity for user

--- lib/features/chat_thread/presentation/cubit/chat_thread_list_cubit.dart
+++ lib/features/chat_thread/presentation/cubit/chat_thread_list_cubit.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../domain/usecases/get_chat_threads_usecase.dart';
+import '../../domain/usecases/create_chat_thread_usecase.dart';
 import 'chat_thread_list_state.dart';
 
 class ChatThreadListCubit extends Cubit<ChatThreadListState> {
   final GetChatThreadsUseCase getChatThreadsUseCase;
+  final CreateChatThreadUseCase createChatThreadUseCase;
 
-  ChatThreadListCubit(this.getChatThreadsUseCase) : super(ChatThreadListInitial());
+  ChatThreadListCubit({
+    required this.getChatThreadsUseCase,
+    required this.createChatThreadUseCase,
+  }) : super(ChatThreadListInitial());
 
   Future<void> fetchChatThreads() async {
     emit(ChatThreadListLoading());
@@ -14,6 +19,26 @@ class ChatThreadListCubit extends Cubit<ChatThreadListState> {
       emit(ChatThreadListLoaded(threads));
     } catch (e) {
       emit(ChatThreadListError(e.toString()));
+    }
+  }
+
+  Future<void> createNewChatThread({
+    required String friendId,
+    required String friendName,
+    required String friendAvatarUrl,
+    String? initialMessage,
+  }) async {
+    try {
+      await createChatThreadUseCase(
+        friendId: friendId,
+        friendName: friendName,
+        friendAvatarUrl: friendAvatarUrl,
+        initialMessage: initialMessage,
+      );
+      // Refresh danh sách sau khi tạo thành công
+      await fetchChatThreads();
+    } catch (e) {
+      emit(ChatThreadListError('Failed to create chat: $e'));
     }
   }
 }
